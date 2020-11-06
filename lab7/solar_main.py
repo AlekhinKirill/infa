316606548ae3204d83edea833257609abd84253b
AlekhinKirill <71757600+AlekhinKirill@users.noreply.github.com>
2020-11-07 00:20:49 +0300

    Update solar_main.py

--- lab7/solar_main.py
+++ lab7/solar_main.py
@@ -75,7 +75,8 @@ def open_file_dialog():
     perform_execution = False
     for obj in space_objects:
         space.delete(obj.image)  # удаление старых изображений планет
-    space_objects = read_space_objects_data_from_file('Text file.txt')
+    in_filename = askopenfilename(filetypes=(("Text file", ".txt"),))
+    space_objects = read_space_objects_data_from_file(in_filename)
     max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in space_objects])
     calculate_scale_factor(max_distance)
 
